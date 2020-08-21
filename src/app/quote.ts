2e432568775f7ccdc889af594f6dca918ef3f52e
p-koskey <patiencekoskey@gmail.com>
2020-08-22 01:32:11 +0300

    initial commit

---
+++ src/app/quote.ts
@@ -0,0 +1,6 @@
+export class Quote {
+    numberOfLikes : number = 0;
+    constructor(public name:string, public quote:string, public author: string, public likes:number){
+
+    }
+}
---
+++ src/app/quote/quote.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import {Quote} from '../quote'
+
+@Component({
+  selector: 'app-quote',
+  templateUrl: './quote.component.html',
+  styleUrls: ['./quote.component.css']
+})
+export class QuoteComponent implements OnInit {
+ 
+  quotes:Quote[]=[
+    
+    new Quote('Kimani', 'If you dont have time to read, you dont have the time (or the tools) to write. Simple as that.',
+    'Stephen King',0),
+    new Quote('Steph', 'To survive, you must tell stories.','Stephen King',0),
+    new Quote('Leo', 'If you dont have time to read, you dont have the time (or the tools) to write. Simple as that.','Umberto Eco',0),
+    new Quote('Tania', 'Words are a lens to focus ones mind.','Ayn Rand',0),
+
+  ]
+
+  upvote(index){
+    this.quotes[index].numberOfLikes++
+  }
+
+  downvote(index){
+    this.quotes[index].numberOfLikes--
+  }
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
