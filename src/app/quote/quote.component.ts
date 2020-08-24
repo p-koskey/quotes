c8cb4fc21da99d2b4699dcf3f477eda9f9a091aa
p-koskey <patiencekoskey@gmail.com>
2020-08-24 14:42:39 +0300

    edited quotes

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -14,9 +14,9 @@ export class QuoteComponent implements OnInit {
     new Quote('Kimani', 'If you dont have time to read, you dont have the time (or the tools) to write. Simple as that.',
     'Stephen King',new Date(2020,3,14)),
     new Quote('Steph', 'To survive, you must tell stories.','Umberto Eco',new Date(2019,1,14)),
-    new Quote('Leo', 'If you dont have time to read, you dont have the time (or the tools) to write. Simple as that.','Umberto Eco',new Date(2014,9,1)),
+    new Quote('Leo', 'Darkness cannot drive out darkness: only light can do that. Hate cannot drive out hate: only love can do that.','Martin Luther King',new Date(2014,9,1)),
     new Quote('Tania', 'Words are a lens to focus ones mind.','Ayn Rand',new Date(2020,5,6)),
-    new Quote('Lisa', 'Hardwork pays','Mahtma Gandi',new Date(2010,4,6)),
+    new Quote('Lisa', 'Be the change that you wish to see in the world.','Mahtma Gandi',new Date(2010,4,6)),
     new Quote('George','If you believe it’ll work out, you’ll see opportunities. If you don’t believe it’ll work out, you’ll see obstacles.','Walter Dyer',new Date(2015,11,20))
 
   ]
